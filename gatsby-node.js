5620f943e11cf110d1caa0a43a89c3de437223ee
tiffany may <tiffyzsmile@gmail.com>
2020-10-07 13:47:57 -0700

    prettier format update

--- gatsby-node.js
+++ gatsby-node.js
@@ -29,7 +29,7 @@ exports.createPages = ({ graphql, actions }) => {
             }
           }
         `
-      ).then(result => {
+      ).then((result) => {
         if (result.errors) {
           console.log(result.errors); // eslint-disable-line no-console
           reject(result.errors);
